52322bbd7f69af88e1b744db15b02ccc40c8bde6
Mark Frankle <mark@frankle.fyi>
2024-11-21 17:19:46 +0100

    fix: add craco for @ imports

--- frontend/src/components/TableAssignments/TableAssignments.tsx
+++ frontend/src/components/TableAssignments/TableAssignments.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react"
-// import { Button } from "@/components/ui/button"
-// import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-// import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { Button } from "../../components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../../components/ui/table"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 
 interface TableAssignment {
