5d8e2029d7d9c1d5da1bb4855df0ac7b05686e39
Mark Frankle <mark@frankle.fyi>
2024-11-21 17:13:30 +0100

    fix: tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Include your source files
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {}, // Extend Tailwind's default theme if needed
+    extend: {},
   },
   plugins: [],
 };
