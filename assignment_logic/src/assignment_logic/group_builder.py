bc32f2206c648b471e211b4ccce0ec875b287f4f
Mark Frankle <mark@frankle.fyi>
2024-11-21 13:48:35 +0100

    feat: add gitignore and make group builder into class

--- assignment_logic/src/assignment_logic/group_builder.py
+++ assignment_logic/src/assignment_logic/group_builder.py
@@ -7,134 +7,116 @@ class GroupBuilder:
         self.participants = participants
         self.tables = range(num_tables)
         self.sessions = range(num_sessions)
-        self.model = cp_model.CpModel()
-        self.table_size = ceil(len(participants) / len(tables))
+        self.table_size = ceil(len(participants) / len(self.tables))
         self.religions = set([p['religion'] for p in participants])
         self.genders = set([p['gender'] for p in participants])
 
     def generate_assignments(self) -> None:
         self.setup_model()
-        print("Imported")
+        self._add_constraints_to_model()
+        self._add_objective_functions_to_model
+        self._run_solver()
     
     def setup_model(self):
+        self.model = cp_model.CpModel()
 
+        # Decision variables
+        # x[p,t,s] -> is [p]articipant sitting at [t]able in [s]ession
+        self.x = {}
+        for p in self.participants:
+            for s in self.sessions:
+                for t in self.tables:
+                    self.x[(p['id'],s,t)] = self.model.NewBoolVar(f"x_p{p['id']}_s{s}_t{t}")
 
-    # Decision variables
-    # x[p,t,s] -> is [p]articipant sitting at [t]able in [s]ession
-    x = {}
-    for p in participants:
-        for s in sessions:
-            for t in tables:
-                x[(p['id'],s,t)] = model.NewBoolVar(f"x_p{p['id']}_s{s}_t{t}")
+    def _add_constraints_to_model(self):
+        # Each participants sits at one table per session
+        for p in self.participants:
+            for s in self.sessions:
+                self.model.Add(
+                    sum(self.x[(p['id'],s,t)] for t in self.tables) == 1
+                )
 
-    # Constraint definitions
-    # Each participants sits at one table per session
-    for p in participants:
-        for s in sessions:
-            model.Add(
-                sum(x[(p['id'],s,t)] for t in tables) == 1
-            )
+        # All tables are within 1 participant of all other tables
+        max_participants = {}
+        min_participants = {}
 
-    # All tables are within 1 participant of all other tables
-    max_participants = {}
-    min_participants = {}
+        for s in self.sessions:
+            max_participants[s] = self.model.NewIntVar(0, len(self.participants), f'max_participants_s{s}')
+            min_participants[s] = self.model.NewIntVar(0, len(self.participants), f'min_participants_s{s}')
 
-    for s in sessions:
-        max_participants[s] = model.NewIntVar(0, len(participants), f'max_participants_s{s}')
-        min_participants[s] = model.NewIntVar(0, len(participants), f'min_participants_s{s}')
+            for t in self.tables:
+                table_participant_count = sum(self.x[(p['id'], s, t)] for p in self.participants)
+                self.model.Add(max_participants[s] >= table_participant_count)
+                self.model.Add(min_participants[s] <= table_participant_count)
 
-        for t in tables:
-            table_participant_count = sum(x[(p['id'], s, t)] for p in participants)
-            model.Add(max_participants[s] >= table_participant_count)
-            model.Add(min_participants[s] <= table_participant_count)
+            self.model.Add(max_participants[s] - min_participants[s] <= 1)
+        
+                # No table has more than one participant from a religion than any other table
+        self._add_participant_attribute_distribution_constraint('religion', self.religions)
+        # No table has more than one participant from a religion than any other table
+        self._add_participant_attribute_distribution_constraint('gender', self.genders)
 
-        model.Add(max_participants[s] - min_participants[s] <= 1)
-
-    def add_participant_attribute_distribution_constraint(attribute_name, attribute_set) -> None:
+    def _add_participant_attribute_distribution_constraint(self, attribute_name, attribute_set) -> None:
         max_participants_per_attribute = {}
         min_participants_per_attribute = {}
 
-        for s in sessions:
+        for s in self.sessions:
             for attribute_value in attribute_set:
-                max_participants_per_attribute[(s,attribute_value)] = model.NewIntVar(0, len(participants),
+                max_participants_per_attribute[(s,attribute_value)] = self.model.NewIntVar(0, len(participants),
                     f'max_participants_{attribute_name}_s{s}_{attribute_name[0]}{attribute_value}')
-                min_participants_per_attribute[(s,attribute_value)] = model.NewIntVar(0, len(participants),
+                min_participants_per_attribute[(s,attribute_value)] = self.model.NewIntVar(0, len(participants),
                     f'min_participants_{attribute_name}_s{s}_{attribute_name[0]}{attribute_value}')
 
-                for t in tables:
+                for t in self.tables:
                     table_participant_count_per_attribute = sum(
-                        x[(p['id'], s, t)] for p in participants if p[attribute_name] == attribute_value
+                        self.x[(p['id'], s, t)] for p in participants if p[attribute_name] == attribute_value
                     )
-                    model.Add(max_participants_per_attribute[(s,attribute_value)] >= table_participant_count_per_attribute)
-                    model.Add(min_participants_per_attribute[(s,attribute_value)] <= table_participant_count_per_attribute)
+                    self.model.Add(max_participants_per_attribute[(s,attribute_value)] >= table_participant_count_per_attribute)
+                    self.model.Add(min_participants_per_attribute[(s,attribute_value)] <= table_participant_count_per_attribute)
 
-                model.Add(max_participants_per_attribute[(s,attribute_value)] - min_participants_per_attribute[(s,attribute_value)] <= 1)
+                self.model.Add(max_participants_per_attribute[(s,attribute_value)] - min_participants_per_attribute[(s,attribute_value)] <= 1)
 
-    # No table has more than one participant from a religion than any other table
-    add_participant_attribute_distribution_constraint('religion', religions)
-    # No table has more than one participant from a religion than any other table
-    add_participant_attribute_distribution_constraint('gender', genders)
+    def _add_objective_functions_to_model(self):
+        # Separate couples as much as possible
+        couples = defaultdict(list)
+        for p in participants:
+            if p['couple_id']:
+                couples[p['couple_id']].append(p)
+                
 
-    # Objective functions
-    # Separate couples as much as possible
-    objective_terms = []
-    couples = defaultdict(list)
-    for p in participants:
-        if p['couple_id']:
-            couples[p['couple_id']].append(p)
-            
+        for s in self.sessions:
+            for t in self.tables:
+                for group in couples.values():
+                    self.model.Add(sum(x[(p['id'], s, t)] for p in group) <= 1)
 
-    for s in sessions:
-        for t in tables:
-            for group in couples.values():
-                model.Add(sum(x[(p['id'], s, t)] for p in group) <= 1)
+        weighted_actual_pairings = 0
+        for s in self.sessions:
+            session_weight = len(self.sessions) - s
+            for t in self.tables:
+                for p1 in participants:
+                    for p2 in participants:
+                        if p2['id'] <= p1['id']:
+                            continue
+                        z = self.model.NewBoolVar(f'pairing_p1{p1["id"]}_p2{p2["id"]}_s{s}_t{t}')
+                        self.model.Add(z <= self.x[(p1['id'], s, t)])
+                        self.model.Add(z <= self.x[(p2['id'], s, t)])
+                        self.model.Add(z >= self.x[(p1['id'], s, t)] + self.x[(p2['id'], s, t)] - 1)
+                        weighted_actual_pairings += session_weight * z
+        self.model.Minimize(weighted_actual_pairings)
 
-    # Distribute religions as much as possible
-    # ideal_religion_distribution = ceil(len(participants) / (len(religions) * len(tables)))
-    # deviations = {}
-    # objective_terms = []
-    # for s in sessions:
-    #     for t in tables:
-    #         for r in religions:
-    #             deviations[(r,s,t)] = model.NewIntVar(0, len(participants), f'deviation_r{r}_s{s}_t{t}')
+    def _run_solver(self):
+        solver = cp_model.CpSolver()
+        status = solver.Solve(self.model)
 
-    #             actual_religion_distribution = sum(x[p['id'],s,t] for p in participants if p['religion'] == r)
-
-    #             model.Add(deviations[(r, s, t)] >= actual_religion_distribution - ideal_religion_distribution)
-    #             model.Add(deviations[(r, s, t)] >= ideal_religion_distribution - actual_religion_distribution)
-
-    #             objective_terms.append(deviations[(r, s,t)])
-    # model.Minimize(sum(objective_terms))
-
-    # Distribute participants as much as possible between sessions, with more emphasis on early sessions
-    weighted_actual_pairings = 0
-    for s in sessions:
-        session_weight = len(sessions) - s
-        for t in tables:
-            for p1 in participants:
-                for p2 in participants:
-                    if p2['id'] <= p1['id']:
-                        continue
-                    z = model.NewBoolVar(f'pairing_p1{p1["id"]}_p2{p2["id"]}_s{s}_t{t}')
-                    model.Add(z <= x[(p1['id'], s, t)])
-                    model.Add(z <= x[(p2['id'], s, t)])
-                    model.Add(z >= x[(p1['id'], s, t)] + x[(p2['id'], s, t)] - 1)
-                    weighted_actual_pairings += session_weight * z
-    model.Minimize(weighted_actual_pairings)
-
-    # Run Solver
-    solver = cp_model.CpSolver()
-    status = solver.Solve(model)
-
-    if status in [cp_model.FEASIBLE, cp_model.OPTIMAL]:
-        print(f'Solution found with total deviation: {solver.ObjectiveValue()}')
-        for s in sessions:
-            for t in tables:
-                for p in participants:
-                    if solver.BooleanValue(x[(p['id'], s, t)]):
-                        print(f"Participant {p['id']} {p['religion']} is at Table {t} in Session {s}.")
-    else:
-        print('No solution')
+        if status in [cp_model.FEASIBLE, cp_model.OPTIMAL]:
+            print(f'Solution found with total deviation: {solver.ObjectiveValue()}')
+            for s in self.sessions:
+                for t in self.tables:
+                    for p in participants:
+                        if solver.BooleanValue(x[(p['id'], s, t)]):
+                            print(f"Participant {p['id']} {p['religion']} is at Table {t} in Session {s}.")
+        else:
+            print('No solution')
 
 if __name__ == "__main__":
     # Data input
@@ -151,5 +133,5 @@ if __name__ == "__main__":
         {'id': 10, 'name': 'Akshay', 'religion': 'Muslim', 'gender': 'Male', 'couple_id': None},
     ]
     dummy_groups = 2
-    result = run_assignments(dummy_students, dummy_groups)
-    print(f"Assignments: {result}")
+    # result = run_assignments(dummy_students, dummy_groups)
+    # print(f"Assignments: {result}")
