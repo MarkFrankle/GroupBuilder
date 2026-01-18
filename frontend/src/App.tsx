2bf58ca228bf6b0d91b946adcdf8f91419a96802
Mark Frankle <markfrankle@Marks-MacBook-Air.local>
2026-01-18 17:20:47 +0100

    feat: add auth context provider for user state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext"
 import LandingPage from "./pages/LandingPage"
 import TableAssignmentsPage from "./pages/TableAssignmentsPage"
 import SeatingChartPage from "./pages/SeatingChartPage"
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/table-assignments" element={<TableAssignmentsPage />} />
-        <Route path="/table-assignments/seating" element={<SeatingChartPage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/table-assignments" element={<TableAssignmentsPage />} />
+          <Route path="/table-assignments/seating" element={<SeatingChartPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
